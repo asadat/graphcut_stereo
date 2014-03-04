a6cedb57114e9764122a2f4169b31a457eb13eb8
Abbas <sas21@sfu.ca>
2014-03-04 15:52:59 -0800

    initial commit

---
+++ src/GraphcutStereo.h
@@ -0,0 +1,13 @@
+#ifndef GRAPH_CUT_STEREO_
+#define GRAPH_CUT_STEREO_
+
+class GraphcutStereo
+{
+public:
+    GraphcutStereo(int argc, char ** argv);
+    ~GraphcutStereo();
+private:
+
+};
+
+#endif
